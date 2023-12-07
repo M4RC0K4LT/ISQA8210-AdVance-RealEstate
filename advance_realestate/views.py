2373564a667f523d4223f71a7104619360fce98b
ValUNO1 <93164480+ValUNO1@users.noreply.github.com>
2023-12-07 11:05:55 -0600

    update about us

--- advance_realestate/views.py
+++ advance_realestate/views.py
@@ -4,17 +4,23 @@ from listings.models import Property, Property_Image
 # Landing Page
 def home_view(request):
     try:
-        #Check if any listing is featured
+        # Check if any listing is featured
         featured_listing = Property.objects.get(property_feature_status=True)
-        images = Property_Image.objects.select_related().filter(property_id = featured_listing)
-        image_urls = []
-        for image in images:
-            image_urls.append(str(image.property_image_location))
+        featured_listing_id = featured_listing.id  # Get the ID of the featured listing to redirect to listing detail page
+        images = Property_Image.objects.select_related().filter(property_id=featured_listing)
+        image_urls = [str(image.property_image_location) for image in images]
     except Property.DoesNotExist:
+        # Handle the case when no featured listing is found
         featured_listing = None
+        featured_listing_id = None
         image_urls = None
-    return render(request, 'home.html', {'listing_featured': featured_listing, 'image_urls' : image_urls})
 
+    # Render the 'home.html' template with the context data
+    return render(request, 'home.html', {
+        'listing_featured': featured_listing,
+        'featured_listing_id': featured_listing_id,
+        'image_urls': image_urls
+    })
 
 # AreaInfo Page
 def area_view(request):   
