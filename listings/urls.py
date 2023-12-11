4738708ccae9c6535e732cf5b60198cbe15641bc
niha009 <122668080+niha009@users.noreply.github.com>
2023-12-12 03:28:27 +0530

    Update urls.py

--- listings/urls.py
+++ listings/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('listings/upload/', views.listings_upload, name='listings_upload'),
     path('listing/<int:listing_id>', views.listing_detail_view, name='listing_detail'),
     path('listing/delete/<int:listing_id>', views.listing_delete, name='listing_delete'),
-    path('listing/feature/<int:listing_id>', views.listing_feature, name='listing_feature'),
-    path('listing/status/<int:listing_id>/<int:status_id>', views.listing_status, name='listing_status'),
+     path('listing/<int:listing_id>/change_status/', views.listing_change_status, name='listing_change_status'),
+    path('listing/<int:listing_id>/add_as_featured/', views.listing_add_as_featured, name='listing_add_as_featured'),
 ]
