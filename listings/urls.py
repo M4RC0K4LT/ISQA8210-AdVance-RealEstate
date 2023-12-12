249b92359bd1352fae27f5db3ef17b7d1fe26993
vinaykumarreddyvavilla <134454919+vinaykumarreddyvavilla@users.noreply.github.com>
2023-12-13 01:53:30 +0530

    Update urls.py

--- listings/urls.py
+++ listings/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('listing/delete/<int:listing_id>', views.listing_delete, name='listing_delete'),
      path('listing/<int:listing_id>/change_status/', views.listing_change_status, name='listing_change_status'),
     path('listing/<int:listing_id>/add_as_featured/', views.listing_add_as_featured, name='listing_add_as_featured'),
+     path('report/', views.generate_pdf_report, name='pdf_report'),
 ]
