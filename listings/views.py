0f4a4c0352d586e993d522d5b8ab9b7738cde746
vinaykumarreddyvavilla <134454919+vinaykumarreddyvavilla@users.noreply.github.com>
2023-12-13 02:14:52 +0530

    Update views.py

--- listings/views.py
+++ listings/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from django.http import JsonResponse
 from .filters import ListingsFilter
 from .forms import AddressUploadForm, ListingImagesUploadForm, ListingUploadForm
