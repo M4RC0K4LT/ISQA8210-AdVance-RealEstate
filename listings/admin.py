93b54cffb8064782191990fb93a680e3f1c423a4
vinaykumarreddyvavilla <134454919+vinaykumarreddyvavilla@users.noreply.github.com>
2023-12-13 21:47:27 +0530

    Update admin.py

--- listings/admin.py
+++ listings/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 
-from listings.models import Property, Property_Address, Property_Status, Property_Price_Range, Property_Image, Property_Neighborhood, Property_Type, Search_Filter
+from listings.models import Property, Property_Address, Property_Status, Property_Price_Range, Property_Image, Property_Neighborhood, Property_Type, Filter1
 
 
 # Registering all models for admin page
-@admin.register(Property, Property_Status, Property_Image, Property_Neighborhood, Property_Type, Property_Price_Range, Property_Address, Search_Filter)
+@admin.register(Property, Property_Status, Property_Image, Property_Neighborhood, Property_Type, Property_Price_Range, Property_Address, Filter1)
 class ListingAdmin(admin.ModelAdmin):
     pass
