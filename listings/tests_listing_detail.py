ecde904abb818c0c0cc9c6c607876e9ac07c37aa
niha009 <122668080+niha009@users.noreply.github.com>
2023-12-14 16:04:56 +0530

    Add files via upload

---
+++ listings/tests_listing_detail.py
@@ -0,0 +1,37 @@
+import unittest
+import time
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
+
+listing_path = "//button[contains(.,'Explore Properties')]"
+contact_path = "//button[contains(.,'CLICK TO CONTACT AGENT TODAY!')]"
+
+class tests_listing_detail(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+
+    def tests_detail(self):
+        driver = self.driver
+        driver.maximize_window()
+        driver.get('http://127.0.0.1:8000/')
+        time.sleep(5)
+        listing_button = driver.find_element(By.XPATH, listing_path)
+        listing_button.click()
+        time.sleep(4)
+        driver.get('http://127.0.0.1:8000/listing/4')
+        time.sleep(4)
+        try:
+            elem = driver.find_element(By.XPATH, contact_path)
+            driver.close()
+            assert True
+
+        except NoSuchElementException:
+            driver.close()
+            self.fail("error occured")
+            driver.close()
+
+
+if __name__ == "__main__":
+    unittest.main()
